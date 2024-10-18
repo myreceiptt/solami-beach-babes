fff1b53c721085d8f0b0763f93e69c5ab28c5541
myreceiptt <nota@straight-line.org>
2024-10-18 15:28:39 +0700

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div
         className="w-full h-full bg-cover bg-top absolute inset-0"
         style={{
-          backgroundImage: `url('/solami-website.jpg')`, // Set the background image
+          backgroundImage: `url('/solami-background.png')`, // Set the background image
         }}
       />
 
@@ -22,7 +22,7 @@ export default function Home() {
             alt="Solami Logo"
             width={63} // Adjusted width
             height={63} // Adjusted height
-            className="rounded-full w-[4vw] h-[4vw] mt-[1vh] sm:mt-[2vh] transition-transform duration-300 ease-in-out transform hover:scale-110" // Add hover effect
+            className="rounded-full w-[4vw] h-[4vw] mt-[2vh] sm:mt-[3vh] transition-transform duration-300 ease-in-out transform hover:scale-110" // Add hover effect
           />
         </Link>
         <Image
@@ -35,44 +35,45 @@ export default function Home() {
       </header>
 
       {/* Main Content */}
-      <main className="flex-grow flex items-center justify-center relative z-10">
-        <div className="h-[720px] border-2 border-black">
-          {/* You can add your main content here */}
-        </div>
-        <p className="text-white text-lg">
-          Welcome to <span className="text-amber-500">Solami Beach Babes</span>.
-          Where the sun is always shining and everyone needs an oily rubdown.
-          Put a few babes in your wallet and enjoy games, customization,
-          tokenomics, random airdrops, and full frontal degeneracy.
-        </p>
+      <main className="flex-grow flex items-center justify-center relative z-10 mt-[15vh]">
+        {/* <div className="border-2 border-black px-60">
+          <h1 className="text-center text-black text-4xl font-black">
+            Welcome to{" "}
+            <span className="text-amber-500">Solami Beach Babes</span>. Where
+            the sun is always shining and everyone needs an oily rubdown. Put a
+            few babes in your wallet and enjoy games, customization, tokenomics,
+            random airdrops, and full frontal degeneracy.
+          </h1>
+        </div> */}
+        {/* You can add your main content here */}
       </main>
 
       {/* Footer */}
-      <footer className="flex justify-center items-center py-4 relative z-10">
+      <footer className="flex justify-center items-center relative z-10">
         <Link
-          href="https://twitter.com"
+          href="https://x.com/SolamiBeachBabe"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             src="/twitter-logo.png" // Twitter logo file name
             alt="Twitter Logo"
-            width={40} // Adjust as needed
-            height={40} // Adjust as needed
-            className="mx-4 transition-transform duration-300 ease-in-out transform hover:scale-110" // Add hover effect
+            width={139} // Adjust as needed
+            height={126} // Adjust as needed
+            className="rounded-full w-[6vw] mx-[4vw] mr-[10vw] mb-[2vh] sm:mb-[3vh] transition-transform duration-300 ease-in-out transform hover:scale-110" // Add hover effect
           />
         </Link>
         <Link
-          href="https://discord.com"
+          href="https://discord.gg/X5QEm6xGsY"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             src="/discord-logo.png" // Discord logo file name
             alt="Discord Logo"
-            width={40} // Adjust as needed
-            height={40} // Adjust as needed
-            className="mx-4 transition-transform duration-300 ease-in-out transform hover:scale-110" // Add hover effect
+            width={123} // Adjust as needed
+            height={114} // Adjust as needed
+            className="rounded-full w-[6vw] mx-[4vw] ml-[10vw] mb-[2vh] sm:mb-[3vh] transition-transform duration-300 ease-in-out transform hover:scale-110" // Add hover effect
           />
         </Link>
       </footer>
